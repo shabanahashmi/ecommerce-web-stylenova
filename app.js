0ab7090dcaabf99b9a926e1de1919ae5ccf7223b
shabanahashmi <shabanahashmi894@gmail.com>
2025-08-29 11:20:35 +0530

    .env

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express= require("express");
 const app =express();
+
+// default module
+const path=require("path")
+
 const env=require("dotenv").config();
 const db=require("./config/db");
-db()
+db();
+
+
+// setup middlewares parse the data
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+// specify view engine
+app.set("view engine","ejs");
+
+// mention views folder
+app.set("views",[path.join(__dirname,'views/user',path.join(__dirname,'views/admin'))]);
+
+// static folder mention
+app.use(express.static(path.join(__dirname,"stylenova")))
 
 
 
